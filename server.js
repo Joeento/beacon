7b83abed6db3f68fa2aa2a87cff5b364b1b69e66
Eric Kudler <joeento@gmail.com>
2014-07-23 16:32:00 -0400

    Added main code for server, complete with routing requests through /apiwq

---
+++ server.js
@@ -0,0 +1,21 @@
+//import classes and setup
+var express    = require('express'); 		// call express
+var app        = express(); 				// define our app using express
+var bodyParser = require('body-parser');
+app.use(bodyParser());
+
+var port = process.env.PORT || 8080; 		// set our port
+
+//add a router
+var router = express.Router(); 				// get an instance of the express Router
+
+//Routes accessed through /api only
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });	
+});
+
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Running on port:  ' + port);
