7df007fdc1d3fa0253d5f26b64636fcbe7db3291
Eric Kudler <joeento@gmail.com>
2014-07-24 23:35:36 -0400

    necessary change to estab table for that last thing I was talking about

--- models/estab.js
+++ models/estab.js
@@ -5,7 +5,8 @@ var EstabSchema   = new Schema({
 	name: String,
 	pin: Number,
 	lat: Number,
-	lon: Number
+	lon: Number,
+	requests: [{songId: String, votes: Number}]
 });
 
 module.exports = mongoose.model('Estab', EstabSchema);
