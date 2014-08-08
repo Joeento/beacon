a9160abbc9dbbcadb566255ce2cba252fa50438f
Eric Kudler <joeento@gmail.com>
2014-08-08 12:32:33 -0400

    Configured homepage and searchpage routing.  Also removed some leftover code from tutorial.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-angular.module('beaconApp', ['beaconControllers', 'beaconServices']);
+var beaconApp = angular.module('beaconApp', ['ngRoute', 'beaconControllers', 'beaconServices']);
+
+beaconApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/search', {
+        templateUrl: 'views/search.html',
+        controller: 'SongSearchCtrl'
+      }).
+      when('/', {
+        templateUrl: 'views/home.html',
+        controller: ''
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
